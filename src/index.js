754816d8a2e901777850be5e88941c12706ce7a3
Tomás Chaves <tomcchaves@gmail.com>
2023-04-14 14:58:38 -0300

    Requisito 1.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 app.use(express.json());
+const fs = require('fs').promises;
+const { join } = require('path');
 
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
@@ -14,3 +16,20 @@ app.get('/', (_request, response) => {
 app.listen(PORT, () => {
   console.log('Online');
 });
+
+
+const path = '/talker.json'
+const joinPath = join(__dirname, path);
+
+app.get('/talker', async(req, res) => {
+  const readTalkers = await fs.readFile(joinPath, 'utf-8');
+  const responseTalkers = await JSON.parse(readTalkers);
+  if (responseTalkers.length === 0) {
+    return res.status(200).json([]);
+  }
+
+  return res.status(200).json(responseTalkers);
+})
+
+
+
