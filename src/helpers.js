dcad5408999a6f78c3bc958626e5726e83dec2ae
Tomás Chaves <tomcchaves@gmail.com>
2023-04-14 16:19:28 -0300

    Requisito 3.

---
+++ src/helpers.js
@@ -0,0 +1,7 @@
+// const validateEmail = (req, res) => {
+//   const { email, password } = req.body;
+//   const regexEmail = /\S+@\S+\.\S+/;
+//   const validateEmail = regexEmail.test(email);
+// }
+
+// module.exports = validateEmail;
--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ const { join } = require('path');
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
 
+const crypto = require('crypto');
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
@@ -21,13 +23,14 @@ app.listen(PORT, () => {
 const path = '/talker.json'
 const joinPath = join(__dirname, path);
 
+
 app.get('/talker', async(req, res) => {
   const readTalkers = await fs.readFile(joinPath, 'utf-8');
   const responseTalkers = await JSON.parse(readTalkers);
   if (responseTalkers.length === 0) {
     return res.status(200).json([]);
   }
-
+  
   return res.status(200).json(responseTalkers);
 })
 
@@ -35,12 +38,43 @@ app.get('/talker/:id', async(req, res) => {
   const readTalkers = await fs.readFile(joinPath, 'utf-8');
   const responseTalkers = await JSON.parse(readTalkers);
   const { id } = req.params;
-  
+
   const searchPerson = responseTalkers.find((talker) => talker.id === +id);
 
   if (!searchPerson) {
     return res.status(404).json({ "message": "Pessoa palestrante não encontrada" });
   }
-
+  
   return res.status(200).json(searchPerson);
 })
+
+
+app.post('/login', /* middleWareDeEmail,  */ async(req, res) => {
+  const readTalkers = await fs.readFile(joinPath, 'utf-8');
+  const responseTalkers = await JSON.parse(readTalkers);
+  const { email, password } = req.body;
+  
+
+
+  const token = crypto.randomBytes(16).toString('base64').slice(0, 16);
+  
+  const tokenResponse = { "token": token }
+
+  return res.status(200).json(tokenResponse);
+  
+  // const regexEmail = /\S+@\S+\.\S+/;
+  // const validateEmail = regexEmail.test(email);
+  // const emailLength = email.length > 0;
+  // const passwordLength = password.length >= 6;
+
+  // o middleware tem acesso à requisição, então não preciso passar parâmetro. o middleware está antes de chegarmos até à função/endpoint de login, então ele não precisa constar no corpo do endpoint em si. colocando next(), ele vai para o próximo, caso houver, sendo especificado logo em seguida a ele na primeira linha. export e importo normalmente como uma função
+  
+  // console.log(validateEmail && emailLength && passwordLength);
+
+
+  // if(email.length > )
+
+
+})
+
+
